64e9646a4ab530812a21ee0cea85e2df94afd913
Matt Williams <matt.k.wililams@protonmail.com>
2023-05-10 19:17:29 -0400

    FIX: nvim-cmp ghost-text bug fixed
    https://github.com/hrsh7th/nvim-cmp/issues/1565

--- nvim/lua/my/configure/completion.lua
+++ nvim/lua/my/configure/completion.lua
@@ -50,7 +50,7 @@ return {
         format = require('lspkind').cmp_format({ with_text = true }),
       },
       experimental = {
-        ghost_text = true,
+        ghost_text = {},
       },
     }
 
