f5ab7904d8d2031f098b5af51cea20f6acf66de6
Matt Williams <matt.k.williams@protonmail.com>
2023-07-11 12:43:26 -0400

    theme: Add kanagawa theme and set as default

--- nvim/lua/my/configure/theme/init.lua
+++ nvim/lua/my/configure/theme/init.lua
@@ -2,30 +2,31 @@
 
 local M = {}
 
--- Loads what ever theme the user wants from this directory
--- `my.plugins.lua`
--- 
+-- Loads what ever theme the user wants from this directory `my.plugins.lua`
+--
 -- Supported theme modules:
 --  * my.configure.theme.onedark
 --  * my.configure.theme.gruvbox
 --  * my.configure.theme.ayu
+--  * my.configure.theme.kanagawa
 --
 -- Returns: table - theme configuration
 function M.setup()
-  return require('my.configure.theme.ayu')
+  return require('my.configure.theme.kanagawa')
 end
 
 -- Returns the name of the theme.
 -- Method is used in `my.configure.lualine.lua`
--- 
--- Supported theme names: 
+--
+-- Supported theme names:
 --  * onedark
 --  * gruvbox
 --  * ayu
+--  * auto
 --
 -- Returns: string - name of the theme
 function M.name()
-  return 'ayu'
+  return 'auto'
 end
 
 return M
