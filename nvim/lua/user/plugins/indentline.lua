8169cc3167f8656a9b7131687683523a84a1e996
Matt Williams <matt.k.williams@protonmail.com>
2023-11-15 10:06:44 -0500

    Port indentline plugin code for version 3

--- nvim/lua/user/plugins/indentline.lua
+++ nvim/lua/user/plugins/indentline.lua
@@ -1,31 +1,11 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = "VeryLazy",
 }
 
 function M.config()
-  local icons = require "user.utils.icons"
-
-  require("indent_blankline").setup {
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-      "help",
-      "startify",
-      "dashboard",
-      "lazy",
-      "neogitstatus",
-      "NvimTree",
-      "Trouble",
-      "text",
-    },
-    char = icons.ui.LineMiddle,
-    context_char = icons.ui.LineMiddle,
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
-    indent = { char = icons.ui.LineMiddle },
-  }
+  require("ibl").setup()
 end
 
 return M
