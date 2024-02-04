d9f795aaa437d9539508f2c0053d42c189d8f864
Matt Williams <matt.k.williams@protonmail.com>
2024-02-04 03:27:26 -0500

    Set more conservative neovide options

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -57,10 +57,11 @@ if vim.g.neovide then
   vim.g.neovide_light_angle_degrees = 45
   vim.g.neovide_light_radius = 5
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_refresh_rate = 144
+  vim.g.neovide_refresh_rate = 120
   vim.g.neovide_refresh_rate_idle = 5
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_cursor_antialiasing = true
-  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_unlink_border_highlights = true
+  vim.g.neovide_remember_window_size = true
 end
