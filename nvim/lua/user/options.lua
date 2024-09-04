640cfe820ec3543ce01924e7ad8512d7d4398a09
Matt Williams <matt.k.williams@protonmail.com>
2024-09-04 13:13:56 -0700

    Increase neovim gui font size

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -35,7 +35,7 @@ local options = {
   linebreak = true, -- companion to wrap, don't split words
   scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
   sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "NotoSansM NFM:h12", -- the font used in graphical neovim applications
+  guifont = "NotoSansM NFM:h17", -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
