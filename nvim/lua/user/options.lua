9db3b7e7faeecf6ed486c1ede87ba4fe5087b938
Matt Williams <matt.k.williams@protonmail.com>
2024-11-20 10:30:30 -0800

    Update all plugins, change neovide gui font to BlexMono [issue]: Multi-line description of commit

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -35,7 +35,7 @@ local options = {
   linebreak = true, -- companion to wrap, don't split words
   scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
   sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "NotoSansM NFM:h13", -- the font used in graphical neovim applications
+  guifont = "BlexMono Nerd Font Mono:h13", -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
